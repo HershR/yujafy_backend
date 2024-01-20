2cf54df05e670fef22d91e99a8e2ffc206906cc9
Hersh Rudrawal <45576093+HershR@users.noreply.github.com>
2024-01-20 00:11:57 -0800

    process func implemented

--- flaskr/api.py
+++ flaskr/api.py
@@ -1,10 +1,15 @@
 import flask
 from flask import Blueprint, request, jsonify
 from .request_utils import SENTENCES, VOICE, check_json_post_args, check_get_args
+import textwrap
+import requests
+import json
 
 bp = Blueprint('api', __name__)
 
-API_KEY = flask.Config['API_KEY']
+API_KEY = "1b547580cfd44b27b1647aec0fafcddc"
+
+
 # FILE_PATH = flask.Config['FILE_PATH']
 
 
@@ -18,7 +23,39 @@ def process_text():
     data, error = check_json_post_args([SENTENCES, VOICE])
     if error:
         return jsonify(error), 400
-    return jsonify({'data': "test.url"}), 200
+    url = "https://api.topmediai.com/v1/text2speech"
+    sentences = request.json.get(SENTENCES)
+    headers = {
+        "accept": "application/json",
+        "x-api-key": API_KEY,
+        "Content-Type": "application/json"
+    }
+    audio_links = []
+    for sentence in sentences:
+        chunks = textwrap.wrap(sentence, width=250)
+
+        links = []
+
+        for chunk in chunks:
+            data = {
+                "text": chunk,
+                "speaker": "0015548d-3826-11ee-a861-00163e2ac61b",
+                "emotion": "Neutral"
+            }
+
+            r = requests.post(url, headers=headers, data=json.dumps(data))
+
+            # The response body is a JSON string, so we parse it into a Python dictionary
+            if r.status_code == 200:
+
+                data = r.json()
+                print(data['data']['oss_url'])
+                links.append(data['data']['oss_url'])
+            else:
+                print(r)
+                links.append(None)
+        audio_links.append(links)
+    return jsonify({'data': audio_links}), 200
 
 
 '''
