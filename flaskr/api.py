9a82ff3901c1d5178a271924cebb54ebd10aea8e
Hersh Rudrawal <45576093+HershR@users.noreply.github.com>
2024-01-20 18:26:21 -0800

    Update api.py

--- flaskr/api.py
+++ flaskr/api.py
@@ -1,17 +1,16 @@
-import flask
 from flask import Blueprint, request, jsonify, current_app
 from flask_cors import CORS, cross_origin
 from .request_utils import ID, SENTENCES, VOICE, check_json_post_args, check_get_args
-import textwrap
 import requests
 import json
 
 bp = Blueprint('api', __name__)
 
-API_KEY = "1b547580cfd44b27b1647aec0fafcddc"
+API_KEY = "06085097256849a3a7b5f837c4583363"
 
-FILE_PATH = './audiofiles'
+FILE_PATH = "./audiofiles"
 
+DEFAULT_VOICE = "0015548d-3826-11ee-a861-00163e2ac61b"
 
 @bp.route('/api/process', methods=['POST'])
 @cross_origin()
@@ -21,51 +20,57 @@ def process_text():
     :param: sentences: List , voice: AI voice
     :return:
     """
-    data, error = check_json_post_args([SENTENCES, VOICE])
+    _, error = check_json_post_args([SENTENCES, VOICE])
     if error:
         return jsonify(error), 400
     url = "https://api.topmediai.com/v1/text2speech"
     v_id = request.json.get(ID)
     sentences = request.json.get(SENTENCES)
-    print(sentences)
+    voice_id = DEFAULT_VOICE if request.json.get(VOICE) == 'default' else request.json.get(VOICE)
+    print(sentences, voice_id)
     headers = {
         "accept": "application/json",
         "x-api-key": API_KEY,
         "Content-Type": "application/json"
     }
-    audio_links = {}
+    audio_links = []
     for sentence in sentences:
-        chunks = textwrap.wrap(sentence, width=250)
+        chunks = split_string(sentence, 200)
 
-        links = []
+        # links = []
 
         for chunk in chunks:
-            data = {
-                "text": chunk,
-                "speaker": "0015548d-3826-11ee-a861-00163e2ac61b",
+            param = {
+                "text": chunk.replace('\ufeff', ''),
+                "speaker": voice_id,
                 "emotion": "Neutral"
             }
 
-            r = requests.post(url, headers=headers, data=json.dumps(data))
+            r = requests.post(url, headers=headers, data=json.dumps(param))
 
             # The response body is a JSON string, so we parse it into a Python dictionary
             if r.status_code == 200:
-
                 data = r.json()
-                print(chunk, data['data']['oss_url'])
-                links.append(data['data']['oss_url'])
+                try:
+                    print(chunk, data['data']['oss_url'])
+                    audio_links.append(data['data']['oss_url'])
+                except BaseException as e:
+                    print(e)
+                    print(chunk)
+                    print('data', data)
             else:
                 print(r)
-                links.append(None)
-        audio_links[len(audio_links)] = links
+                # links.append(None)
+        # audio_links[len(audio_links)] = links
     file_name = f'{FILE_PATH}\{v_id}.json'
 
-    #import os
-    #current_directory = os.getcwd()
-    #print(f"The current working directory is: {current_directory}")
+    # import os
+    # current_directory = os.getcwd()
+    # print(f"The current working directory is: {current_directory}")
 
     with open(file_name, 'w') as json_file:
         json.dump(audio_links, json_file, indent=4)
+
     return jsonify({'data': audio_links}), 200
 
 
@@ -89,4 +94,13 @@ def retrieve_audio():
         return jsonify("error: Unable to find file audio files corresponding to ID"), 404
     return jsonify({'data': audio_links}), 200
 
+
 # /v1/voices_list
+
+def split_string(input_string, max_length):
+    result = []
+
+    for i in range(0, len(input_string), max_length):
+        result.append(input_string[i:i + max_length])
+
+    return result
